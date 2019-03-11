c86200361b708bd5a3471f06157f49c782a780a3
Ilia Pochinskii <ilia.pochinskii@stayfriends.com>
2019-03-11 03:35:34 +0100

    add an integration test for the app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('Component: App', () => {
+    it('should deeply render without crashing', () => {
+        const div = document.createElement('div');
+        const dummyStore = createStore(state => ({}));
+        const wrappedComponenet = (
+            <Provider store={dummyStore}>
+                <App />
+            </Provider>
+        );
+
+        ReactDOM.render(wrappedComponenet, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
